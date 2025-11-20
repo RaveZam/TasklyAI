fe825db226dcb9ecd9fcfaa8617e3b300d0173c7
Duke <114975455+RaveZam@users.noreply.github.com>
2025-11-20 17:25:33 +0800

    Fixed Logo Filepath Issue

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -518,7 +518,7 @@ export function Sidebar() {
       >
         <div className="relative flex-shrink-0 ">
           <Image
-            src="/logo/Darkmode.png"
+            src="/logo/Darkmode.ico"
             alt="TasklyAI Logo"
             width={32}
             height={32}
@@ -526,7 +526,7 @@ export function Sidebar() {
             priority
           />
           <Image
-            src="/logo/Lightmode.png"
+            src="/logo/Lightmode.ico"
             alt="TasklyAI Logo"
             width={32}
             height={32}
